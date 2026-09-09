39d359846ca8a26167eef76a7079b3e778432d9b
MariaGabrielaRibeiro <mariaribeiro49170@gmail.com>
2026-09-09 12:17:40 -0300

    Exercicios Javascript Aula 1

--- aula-02/ex04.js
+++
@@ -1,4 +0,0 @@
-const nome = "Maria Gabriela Ribeiro dos Santos"; 
-console.log(`Meu nome possui ${nome.length} letras`); 
-console.log(`Meu nome em capslock: ${nome.toUpperCase()}`); 
-console.log(`Meu nome possui a letra 'a'? ${nome.includes('a')}`);
