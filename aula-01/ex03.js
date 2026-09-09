1dc41fe9025e25d58a129a65f7c4f122a5d90c02
MariaGabrielaRibeiro <mariaribeiro49170@gmail.com>
2026-09-09 12:22:41 -0300

    Exercícios Javascript Aula 1

---
+++ aula-01/ex03.js
@@ -0,0 +1,6 @@
+// ex03.js - Quadrado de asteriscos ((exatamente 5 console.log))
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
---
+++ aula-01/ex04.js
@@ -0,0 +1,11 @@
+// ex04.js - Provocando um erro
+
+console.log(variavelQueNaoExiste); 
+
+/* 
+MENSAGEM DE ERRO COPIADA:
+ReferenceError: variavelQueNaoExiste is not defined
+
+EXPLICAÇÃO:
+Este erro ocorre porque tentei usar ou exibir uma variável chamada 'variavelQueNaoExiste' que nunca foi declarada ou criada antes no código. O JavaScript não sabe o que ela significa.
+*/
