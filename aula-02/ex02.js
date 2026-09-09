3c98dbc50ae1e4fa756f220407b01dc7afd3c0ae
MariaGabrielaRibeiro <mariaribeiro49170@gmail.com>
2026-09-09 12:24:46 -0300

    Exercícios Javascript Aula 2

---
+++ aula-02/ex02.js
@@ -0,0 +1,7 @@
+const anoNascimento = 2007;
+
+const idade2026 = 2026 - anoNascimento;
+const idade2050 = 2050 - anoNascimento;
+
+console.log(`Em 2026 eu terei ${idade2026} anos.`);
+console.log(`Em 2050 eu terei ${idade2050} anos.`);
